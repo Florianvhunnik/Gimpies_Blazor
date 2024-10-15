9d7e4d9b3cd77c91604685a60b9046000493aadb
Florian <florian2006email@gmail.com>
2024-10-15 12:31:07 +0200

    Create Users model

---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+namespace Gimpies_Blazor.Models;
+
+public class Users
+{
+    public int GebruikerId { get; set; }
+    public string? Gebruikersnaam { get; set; }
+    public string? Wachtwoord { get; set; }
+    public string? Rol { get; set; }
+}
